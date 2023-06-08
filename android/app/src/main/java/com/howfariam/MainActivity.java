d7b456eeb4f2c13e6e45e8b6c41cbbc159fa4f8f
Jérémy Piorun <jpiorun@insitaction.com>
2023-06-08 15:47:15 +0200

    Rename app

--- android/app/src/main/java/com/howfariam/MainActivity.java
+++ android/app/src/main/java/com/howfariam/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "HowFarIam";
+    return "The Place to Be";
   }
 
   /**
